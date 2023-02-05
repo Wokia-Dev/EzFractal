32850ce90932954772c897ad9ae3f69470626675
Wokia-Dev <quentin.ruhf@gmail.com>
2023-02-05 22:34:20 +0100

    change the iter color function

--- Core/EzUtils.py
+++ Core/EzUtils.py
@@ -1,4 +1,5 @@
 import numba
+import numpy
 from numpy import uint8
 
 
@@ -20,21 +21,23 @@ def clamp(value: float, min_value: float, max_value: float) -> float:
 def iter_gradient_generator(num_iter, max_iter) -> [uint8, uint8, uint8]:
     if num_iter == max_iter:
         return [0, 0, 0]
-    elif num_iter < max_iter / 3:
-        return [
-            uint8(255 * (num_iter / (max_iter / 3))),
-            0,
-            255,
-        ]
-    elif num_iter < 2 * max_iter / 3:
-        return [
-            255,
-            uint8(255 * (1 - ((num_iter - max_iter / 3) / (max_iter / 3)))),
-            255,
-        ]
     else:
-        return [
-            255,
-            255,
-            uint8(255 * ((num_iter - 2 * max_iter / 3) / (max_iter / 3))),
-        ]
+        h = num_iter / max_iter
+        saturation = 1
+        lightness = 0.5
+        c = (1 - numpy.abs(2 * lightness - 1)) * saturation
+        x = c * (1 - numpy.abs((h * 6) % 2 - 1))
+        m = lightness - c / 2
+        if 0 <= h < 1 / 6:
+            r, g, b = c + m, x + m, m
+        elif 1 / 6 <= h < 1 / 3:
+            r, g, b = x + m, c + m, m
+        elif 1 / 3 <= h < 1 / 2:
+            r, g, b = m, c + m, x + m
+        elif 1 / 2 <= h < 2 / 3:
+            r, g, b = m, x + m, c + m
+        elif 2 / 3 <= h < 5 / 6:
+            r, g, b = x + m, m, c + m
+        else:
+            r, g, b = c + m, m, x + m
+    return [int(r * 255), int(g * 255), int(b * 255)]
